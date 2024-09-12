7d56073d2382764268c7383040685f5b66a09293
Oshadha_Dhananjaya <dhananjayaoshadha815@gmail.com>
2024-09-12 18:00:30 +0530

    cal

---
+++ cal.py
@@ -0,0 +1,14 @@
+num1=int(input("firstr number"));
+op=input("enter your operation");
+num2=int(input("second number"));
+
+
+
+if op=="+":
+    print(num1+num2);
+elif op=="-":
+    print(num1-num2);
+elif op=="/":
+    print(num1/num2);
+else:
+    print(num1*num2);
